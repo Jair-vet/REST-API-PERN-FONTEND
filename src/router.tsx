6fa040acb43c63de0c5492d5c392e4e8cca1a3d5
Jair <jairaceves56@gmail.com>
2024-04-30 18:35:00 -0600

    Mostramos los Productos

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Layout } from './layouts/Layout'
-import { Products } from './views/Products'
 import { NewProduct, action as newProductAction } from './views/NewProduct'
+import { Products, loader as productsLoader } from './views/Products'
+import EditProduct from './views/EditProduct'
 
 
 export const router = createBrowserRouter([
@@ -11,13 +12,25 @@ export const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Products />
+                element: <Products />,
+                loader: productsLoader,
+                // action: updateAvailabilityAction
             },
             {
                 path: 'productos/nuevo',
                 element: <NewProduct />,
                 action: newProductAction
-            }
+            },
+            {
+                path:'productos/:id/editar', // ROA Pattern - Resource-oriented design
+                element: <EditProduct />,
+                // loader: editProductLoader,
+                // action: editProductAction
+            }, 
+            // {
+            //     path:'productos/:id/eliminar',
+            //     action: deleteProductAction
+            // }
         ]
     }
-])
+]) 
