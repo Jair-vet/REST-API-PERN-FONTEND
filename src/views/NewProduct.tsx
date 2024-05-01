a92b83de12b354614f44edd45b98e7a0273e2ca6
Jair <jairaceves56@gmail.com>
2024-04-30 18:34:41 -0600

    Agregar nuevo Producto

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,12 +1,12 @@
 import { Link, Form, useActionData, ActionFunctionArgs, redirect } from "react-router-dom"
 import { ErrorMessage } from "../components/ErrorMessage"
 import { addProduct } from "../services/ProductService"
+import ProductForm from "../components/ProductForm"
 
 export const action = async ({request} : ActionFunctionArgs) => {
+    
     const data = Object.fromEntries(await request.formData())  // Obtener los datos del Formulario
-
     let error = ''
-
     if(Object.values(data).includes('')){
         error = 'Todos los Campos son Obligatorios'
     }
@@ -14,7 +14,6 @@ export const action = async ({request} : ActionFunctionArgs) => {
     if(error.length){
         return error
     }
-
     await addProduct(data)
 
     // Redireccionar a la Página principal
@@ -45,32 +44,8 @@ export const NewProduct = () => {
             method='POST'    
         >
         
-            <div className="mb-4">
-                <label
-                    className="text-gray-800"
-                    htmlFor="name"
-                >Nombre Producto:</label>
-                <input 
-                    id="name"
-                    type="text"
-                    className="mt-2 block w-full p-3 bg-gray-50"
-                    placeholder="Nombre del Producto"
-                    name="name"
-                />
-            </div>
-            <div className="mb-4">
-                <label
-                    className="text-gray-800"
-                    htmlFor="price"
-                >Precio:</label>
-                <input 
-                    id="price"
-                    type="number"
-                    className="mt-2 block w-full p-3 bg-gray-50"
-                    placeholder="Precio Producto. ej. 200, 300"
-                    name="price"
-                />
-            </div>
+            <ProductForm />
+
             <input
             type="submit"
             className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
