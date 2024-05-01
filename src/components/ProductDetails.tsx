c9cbdee19f12cab8b6cefc1468930049e02fb139
Jair <jairaceves56@gmail.com>
2024-04-30 18:34:21 -0600

    Detalle del Producto

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -45,7 +45,7 @@ export default function ProductDetails({product} : ProductDetailsProps) {
                 <div className="flex gap-2 items-center">
                     <button
                         onClick={() => navigate(`/productos/${product.id}/editar`)}
-                        className='bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center'
+                        className='bg-indigo-600 hover:bg-indigo-700 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center'
                     >Editar</button>
 
                     <Form
